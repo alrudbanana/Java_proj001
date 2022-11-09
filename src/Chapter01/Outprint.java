8230d86bb5023aeb710cdf5e15405e035130258e
migyeong Lee <pratt3343@gmail.com>
2022-11-09 14:20:34 +0900

    첫번째커밋

---
+++ src/Chapter01/Outprint.java
@@ -0,0 +1,11 @@
+package Chapter01;
+
+public class Outprint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("오늘의 %s는 매우 맑고 온도는 %d도다", "날씨", 25 );
+		
+	}
+
+}
